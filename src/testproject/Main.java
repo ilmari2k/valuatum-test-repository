e5d692cb2535a5fa6b115c852e0b37f9dd15641c
Ilmari Kontulainen <ilmari@valuatum.com>
2010-06-02 09:41:08 +0300

    print some more hello worlds + added NewClass to extend our Main class

--- src/testproject/Main.java
+++ src/testproject/Main.java
@@ -16,6 +16,11 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
         // TODO code application logic here
     }
 
