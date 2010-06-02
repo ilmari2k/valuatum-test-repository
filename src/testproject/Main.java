5f7a65e87e3a39dc70922696e83e37521cc60bc2
Ilmari Kontulainen <ilmari@valuatum.com>
2010-06-02 09:51:23 +0300

    hello to sun

--- src/testproject/Main.java
+++ src/testproject/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello sun");
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
